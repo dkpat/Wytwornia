994deb3a698f11b5dfef5906d26dd07c917c4482
marprz26 <marprz26@gmial.com>
2015-03-23 13:56:41 +0100

    Podstawowa konfiguracja modeli i uruchomienie sekcji About

---
+++ Wytwornia/module/About/src/About/Controller/AboutController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Description of AboutController
+ *
+ * @author Marcin
+ */
+
+namespace About\Controller;
+
+ use Zend\Mvc\Controller\AbstractActionController;
+ use Zend\View\Model\ViewModel;
+
+class AboutController extends AbstractActionController {
+    
+    public function indexAction() {
+        
+    }
+}
